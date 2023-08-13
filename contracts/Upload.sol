29618f8f821776fee192f41f3838c47bde385d06
DipakKumarChauhan <ece22133@iiitkalyani.ac.in>
2023-08-13 11:15:46 +0530

    Frontend and User Interface added

--- contracts/Upload.sol
+++ contracts/Upload.sol
@@ -1,50 +1,75 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
-pragma solidity >=0.7.0 <0.9.0;
+contract Upload
+{
+    struct Access
+    {
+        address user;
+        bool access;
+    }
+    //creating data base to store data
+    mapping(address=>string[] )value;
+    mapping(address=>mapping(address=>bool))ownership;
+    mapping(address=>Access[])accessList;
+    mapping(address=>mapping(address=>bool))previousData;
+    //Function to add user data and url 
+    function add( address _user, string memory url) external
+    {
+        value[_user].push(url);
 
-contract Upload {
-  
-  struct Access{
-     address user; 
-     bool access; //true or false
-  }
-  mapping(address=>string[]) value;
-  mapping(address=>mapping(address=>bool)) ownership;
-  mapping(address=>Access[]) accessList;
-  mapping(address=>mapping(address=>bool)) previousData;
+    }
+    //function to allow access to other users
+    function allow(address user) external 
+    {
+        ownership[msg.sender][user] = true;//here msg.sender us user ka address daal raha hai jo smart contract access kar raha hai aur user hamara added user addres hai
+        if(previousData[msg.sender][user])//this works coz boolean value is stored in it
+        {
+            for(uint i =0;i<accessList[msg.sender].length;i++ )
+            {
+                if(accessList[msg.sender][i].user ==user)
+                {
+                    accessList[msg.sender][i].access = true;
+                }
+            }
 
-  function add(address _user,string memory url) external {
-      value[_user].push(url);
-  }
-  function allow(address user) external {//def
-      ownership[msg.sender][user]=true; 
-      if(previousData[msg.sender][user]){
-         for(uint i=0;i<accessList[msg.sender].length;i++){
-             if(accessList[msg.sender][i].user==user){
-                  accessList[msg.sender][i].access=true; 
-             }
-         }
-      }else{
-          accessList[msg.sender].push(Access(user,true));  
-          previousData[msg.sender][user]=true;  
-      }
-    
-  }
-  function disallow(address user) public{
-      ownership[msg.sender][user]=false;
-      for(uint i=0;i<accessList[msg.sender].length;i++){
-          if(accessList[msg.sender][i].user==user){ 
-              accessList[msg.sender][i].access=false;  
-          }
-      }
-  }
+        }
+        else 
+        {
+            accessList[msg.sender].push (Access(user,true));
+            previousData[msg.sender][user] = true;
+        }
 
-  function display(address _user) external view returns(string[] memory){
-      require(_user==msg.sender || ownership[_user][msg.sender],"You don't have access");
-      return value[_user];
-  }
+       
+        
 
-  function shareAccess() public view returns(Access[] memory){
-      return accessList[msg.sender];
-  }
+    }
+    function disAllow(address user)external
+    {
+        ownership[msg.sender][user] = false;
+        for(uint i =0;i<accessList[msg.sender].length;i++ )
+        {
+            if(accessList[msg.sender][i].user ==user)
+            {
+                accessList[msg.sender][i].access = false;
+            }
+        }
+    }
+
+    function display(address _user) view external returns(string[] memory) 
+    {
+        require(_user == msg.sender || ownership[_user][msg.sender],"You don't have the access");
+        return value[_user];
+    }
+
+    function shareAddress() public view returns(Access[] memory)
+    {
+        return accessList[msg.sender];
+    }
+
+
+
+
+
+
 }
